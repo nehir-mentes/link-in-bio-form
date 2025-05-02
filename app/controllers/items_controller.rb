19bdd4b0f2f3c57c3200bce5a044877281d27da9
nehir-mentes <nehirmen.98@gmail.com>
2025-05-02 19:49:43 +0000

    final

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,12 @@ class ItemsController < ApplicationController
   end
 
   def insert
-    render({ :template => "item_templates/insert" })
+    i = Item.new
+    i.link_url = params.fetch("query_link_url")
+    i.link_description = params.fetch("query_link_description")
+    i.thumbnail_url = params.fetch("query_thumbnail_url")
+    i.save
+
+    redirect_to("/")
   end
 end
